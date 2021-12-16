fbc578a960eb24feaa5eed097971260e8a864280
Crutoi Alexandru <crutoi2001@gmail.com>
2021-12-16 16:23:44 +0200

    Finish contact me page

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,7 +1,6 @@
 import "./css/ContactMe.css"
 
 import React from 'react'
-import { Link } from "react-router-dom";
 
 export default function ContactMe() {
     return (
@@ -10,7 +9,12 @@ export default function ContactMe() {
           <h1 >Contact me</h1>
         </div>
         <div className="cards-wrapper__contact-me" >
-       
+          <a href="https://gmail.com" className="cards-email">
+            <p >Contact me on my email at: crutoi2001@gmail.com</p>
+          </a>
+          <a href="https://www.linkedin.com/in/alexandru-crutoi-12794320a" className="cards-linkedin">
+            <p>Contact me on my linkedin</p>
+          </a>
         </div>
         </>
     )
