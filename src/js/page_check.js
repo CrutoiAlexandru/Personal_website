baba7f77b09761e632cfc978566cb6ca9269efd6
CrutoiAlexandru <crutoi2001@gmail.com>
2022-05-25 20:15:24 +0300

    Add multiple pages

---
+++ src/js/page_check.js
@@ -0,0 +1,17 @@
+let el = document.getElementsByClassName("home__top_bar__buttons--each");
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+// color with white the certain page (button) we are on
+if (page == "index.html") {
+  el[0].style.color = "white";
+}
+
+if (page == "about_me.html") {
+  el[1].style.color = "white";
+}
+
+if (page == "contact_me.html") {
+  el[2].style.color = "white";
+}
